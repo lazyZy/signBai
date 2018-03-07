0402149d2e38849a94a61be6a6a4040b2780af04
ZY <746234304@qq.com>
2018-03-07 22:19:20 +0800

    初始化项目

---
+++ volunteer/src/main/java/com/zcz/www/VolunteerApplication.java
@@ -0,0 +1,12 @@
+package com.zcz.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerApplication.class, args);
+	}
+}
