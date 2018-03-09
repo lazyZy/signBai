bfb6e2189a1b5edc9844288769d900d812b5be10
ZY <746234304@qq.com>
2018-03-09 18:22:15 +0800

    架构搭建

--- volunteer/src/main/java/com/zcz/www/VolunteerApplication.java
+++ volunteer/src/main/java/com/zcz/www/VolunteerApplication.java
@@ -2,7 +2,9 @@ package com.zcz.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource(locations = {"classpath:spring/spring-all.xml"})
 @SpringBootApplication
 public class VolunteerApplication {
 
